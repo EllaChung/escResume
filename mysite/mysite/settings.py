087090ceeee91192dd8ebbd076ef717328cc9278
Sammie (Ella) Chung <sc6aa@virginia.edu>
2020-05-06 20:11:42 -0400

    added try except

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -124,4 +124,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-django_heroku.settings(locals())
+try:
+    # Configure Django App for Heroku.
+    import django_heroku
+    django_heroku.settings(locals())
+except ImportError:
+    found = False
