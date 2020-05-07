94d6c761d98943dc336e18fa7bfc95588b0ac276
Sammie (Ella) Chung <sc6aa@virginia.edu>
2020-05-07 00:18:44 -0400

    Set up basic structure

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView, ListView, DetailView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('polls/', include('polls.urls')),
+    path('', TemplateView.as_view(template_name="polls/homepage.html")),
+    path('homepage/', TemplateView.as_view(template_name="polls/homepage.html")),
+    path('aboutme/', TemplateView.as_view(template_name="polls/aboutme.html")),
+    path('previousWork/', TemplateView.as_view(template_name="polls/previousWork.html")),
+    path('courseWork/', TemplateView.as_view(template_name="polls/courseWork.html")),
 ]
